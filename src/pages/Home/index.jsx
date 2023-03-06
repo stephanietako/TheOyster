fef29c6da759e7d8dae61b0b1fbbc84b48d5b16b
stephanietako <gabeshine@live.fr>
2023-03-06 23:24:58 +0100

    starting project

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+// Styles
+import styles from "./styles.module.scss";
+
+const HomePage = () => {
+  return <section className={styles.section}></section>;
+};
+
+export default HomePage;
