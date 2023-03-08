e90f7483babef6fe2303ec8313e80d82d25a783c
stephanietako <gabeshine@live.fr>
2023-03-09 00:34:27 +0100

    wip: Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 // Styles
 import styles from "./styles.module.scss";
-import logoImg from "../../assets/logo/TheOyster_logo_inline-white.png";
+import logoImg from "../../assets/logo/oyster.png";
 
 const Navbar = () => {
   return (
-    <div className={styles.nav_container}>
-      <div className={styles.__box}>
-        <div className={styles.__left}>
+    <nav>
+      <div className={styles.nav_container}>
+        <div className={styles.__logo}>
           <img src={logoImg} alt="logo The Oyster" />
-          <p>LAB</p>
+          <div className={styles.__left}>
+            <p>LAB</p>
+          </div>
         </div>
-        <div className={styles.__right}>
-          <p>GET IN TOUCH</p>
-          <p>THE OYSTER GALLERY</p>
+
+        <div className={styles.__links}>
+          <ul>
+            <li>
+              <a href="/"> GET IN TOUCH</a>
+            </li>
+            <li>
+              <a href="/"> THE OYSTER GALLERY</a>
+            </li>
+          </ul>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
